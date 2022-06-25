5b08824cea4b972e7097216110178958d62eee70
ThaliaLevy <thalialevy@hotmail.com>
2022-06-24 21:53:55 -0300

    aula 174: classe de exceção do chess e classe que converte posição de xadrez para posição de matriz

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,7 +1,6 @@
 package chess;
 
 import boardgame.Board;
-import boardgame.Position;
 import chess.piece.King;
 import chess.piece.Rook;
 
@@ -29,10 +28,15 @@ public class ChessMatch {
 		return mat;
 	}
 	
+	//colocar peça passando as coordenadas do xadrez. ex.: b2
+	private void placeNewPiece(char column, int row, ChessPiece piece) {
+		board.placePiece(piece, new ChessPosition(column, row).toPosition());
+	}
+	
 	//iniciar a partida
 	private void initialSetup() {
-		board.placePiece(new Rook(board,  Color.WHITE), new Position(2, 1));
-		board.placePiece(new King(board,  Color.BLACK), new Position(6, 4));
-		board.placePiece(new King(board,  Color.WHITE), new Position(0, 1));
+		placeNewPiece('b', 6, new Rook(board,  Color.WHITE));
+		placeNewPiece('e', 8, new King(board,  Color.BLACK));
+		placeNewPiece('e', 1, new King(board,  Color.WHITE));
 	}
 }
---
+++ src/chess/ChessPosition.java
@@ -0,0 +1,39 @@
+package chess;
+
+import boardgame.Position;
+
+public class ChessPosition {
+	//Classe que mexe com a posição do xadrez
+	private char column;
+	private int row;
+	public ChessPosition(char column, int row) {
+		if(column < 'a' || column > 'h' || row < 1 || row > 8) {
+			throw new ChessException("Error instantiating ChessPosition. Valid values are from a1 to h8.");
+		}
+		this.column = column;
+		this.row = row;
+	}
+	public char getColumn() {
+		return column;
+	}
+	
+	public int getRow() {
+		return row;
+	}
+	
+	//Convertendo posição de xadrez para posição de matriz
+	protected Position toPosition() {
+		return new Position (8 - row, column - 'a');
+	}
+	
+	//Convertendo posição de matriz para posição de xadrez
+	protected static ChessPosition fromPosition(Position position) {
+		return new ChessPosition((char)('a' - position.getColumn()), 8 - position.getRow());
+	}
+	
+	@Override
+	public String toString() {
+		// "" foi acrescentado para o compilador entender que é uma concatenação de strings
+		return "" + column + row;
+	}
+}
