296d60511cefc4c5d2f62141866756bdd046cc4f
Thalia Levy <69092295+ThaliaLevy@users.noreply.github.com>
2022-08-07 15:59:17 -0300

    aula 201: ontagem de movimento das peças

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -8,6 +8,7 @@ public abstract class ChessPiece extends Piece{
 	//algumas informações sobre as peças
 	
 	private Color color;
+	private int moveCount;	//por padrão começa com 0
 
 	public ChessPiece(Board board, Color color) {
 		super(board);
@@ -17,6 +18,18 @@ public abstract class ChessPiece extends Piece{
 	public Color getColor() {
 		return color;
 	}	
+	
+	public int getMoveCount() {
+		return moveCount;
+	}
+	
+	public void increaseMoveCount() {
+		moveCount++;
+	}
+	
+	public void decreaseMoveCount() {
+		moveCount--;
+	}
 	
 	//pega o retorno da função fromPosition que recebe posição de matriz e retorna em posição de xadrez
 	public ChessPosition getChessPosition() {
