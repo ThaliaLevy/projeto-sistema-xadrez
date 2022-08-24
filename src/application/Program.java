777b9a7250df2991378e83182f0e91d45b908f01
Thalia Levy <69092295+ThaliaLevy@users.noreply.github.com>
2022-08-23 22:01:27 -0300

    aula 210: jogada especial - promoção

--- src/application/Program.java
+++ src/application/Program.java
@@ -38,6 +38,12 @@ public class Program {
 				if (capturedPiece != null) {
 					captured.add(capturedPiece);
 				}
+				
+				if(chessMatch.getPromoted() != null) {
+					System.out.println("Enter piece for promotion (B/C/R/Q): ");
+					String type = sc.nextLine();
+					chessMatch.replacePromotedPlace(type);
+				}
 			} catch (ChessException e) {
 				System.out.println(e.getMessage());
 				sc.nextLine();
